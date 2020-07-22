2819e047e3501bc1303ac8cb35b2e8059315c24d
Reece Mercer <reecemercer981@gmail.com>
2020-07-23 00:34:16 +0100

    Some small layout changes

--- mertfetch.py
+++ mertfetch.py
@@ -9,11 +9,11 @@ print(
     colored(figlet_format('MERTFETCH'), 'green') +
     colored(platform.node(), 'red') + "\n" +
     len(platform.node())*"-" + "\n" +
-      colored("System: ", 'green' ) + platform.platform() + " " + platform.system() +
+      colored("System: ", 'green' ) + platform.platform() + " | " + platform.system() +
       colored("\nPython version: ", 'green'), platform.python_version() +
       colored("\nCPU count: ", 'green') + str(psutil.cpu_count(logical=False)) +
       colored("\nCPU freq: ", 'green') + str(psutil.cpu_freq()[2]) +
-      colored("\nRam: ", 'green') + str(psutil.virtual_memory()[0]/1024**3)[:2] + "GB" +
+      colored("\nRAM: ", 'green') + str(psutil.virtual_memory()[0]/1024**3)[:2] + "GB" +
       colored("\nUptime: ", 'green') + str(datetime.now() - datetime.fromtimestamp(psutil.boot_time()))
 )
 
