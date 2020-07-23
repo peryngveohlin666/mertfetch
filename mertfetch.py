8e0df52c2e56d0b227133f346a2ff08b1c4f140f
Satania Chan <46087170+peryngveohlin666@users.noreply.github.com>
2020-07-23 01:15:47 +0100

    used ram in gb, better looks

--- mertfetch.py
+++ mertfetch.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import platform
 from datetime import datetime
 import psutil
@@ -12,8 +14,8 @@ print(
       colored("System: ", 'green' ) + platform.platform() + " | " + platform.system() +
       colored("\nPython version: ", 'green'), platform.python_version() +
       colored("\nCPU count: ", 'green') + str(psutil.cpu_count(logical=False)) +
-      colored("\nCPU freq (max): ", 'green') + str(psutil.cpu_freq()[2]) + colored(" Current: ", 'yellow') + str(psutil.cpu_freq()[0]) +
-      colored("\nRAM: ", 'green') + str(psutil.virtual_memory()[0]/1024**2)[:2] + "GB" + colored(" Used (%): ", 'yellow') + str(psutil.virtual_memory().percent) +
+      colored("\nCPU freq (Max): ", 'green') + str(psutil.cpu_freq()[2]) + " |" + colored(" Current: ", 'yellow') + str(psutil.cpu_freq()[0]) +
+      colored("\nRAM: ", 'green') + str(psutil.virtual_memory()[0]/1024**2)[:2] + "GB" + " |" + colored(" Used: ", 'yellow') + str(psutil.virtual_memory().percent * psutil.virtual_memory()[0] / 100 / 1024**3)[:4] +
       colored("\nUptime: ", 'green') + str(datetime.now() - datetime.fromtimestamp(psutil.boot_time()))
 )
 
